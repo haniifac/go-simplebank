37d89e1d3f918d155f6a8a1a34c7fe50b2156d80
Haniif Ahmad C <haniifcandra@gmail.com>
2025-03-11 10:26:28 +0700

    latest update

---
+++ api/accounts.go
@@ -0,0 +1,87 @@
+package api
+
+import (
+	"database/sql"
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/haniifac/simplebank/db/sqlc"
+)
+
+type CreateAccountParams struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=EUR USD"`
+}
+
+type GetAccountParams struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+type ListAccountsParams struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req CreateAccountParams
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req GetAccountParams
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			ctx.JSON(http.StatusNotFound, errResponse(err))
+		default:
+			ctx.JSON(http.StatusInternalServerError, errResponse(err))
+		}
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req ListAccountsParams
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: req.PageSize * (req.PageID - 1),
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+
+}
---
+++ api/server.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/haniifac/simplebank/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{
+		store: store,
+	}
+	router := gin.Default()
+
+	// router
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts", server.listAccounts)
+	router.GET("/accounts/:id", server.getAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	err := server.router.Run(address)
+	return err
+}
+
+func errResponse(err error) gin.H {
+	return gin.H{
+		"error": err.Error(),
+	}
+}
