59454f0e61ee1f856c4c75d0ab87507410fb7532
Haniif Ahmad C <haniifcandra@gmail.com>
2025-05-01 20:19:06 +0800

    feat: implement transfer endpoints with custom currency validator

--- api/accounts.go
+++ api/accounts.go
@@ -11,7 +11,7 @@ import (
 
 type CreateAccountParams struct {
 	Owner    string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required,oneof=EUR USD"`
+	Currency string `json:"currency" binding:"required,currency"`
 }
 
 type GetAccountParams struct {
