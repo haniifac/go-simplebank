6b2d1abf7b31f7346a38d21b9e903816d43d966c
Haniif Ahmad C <haniifcandra@gmail.com>
2025-05-26 18:29:39 +0800

    fix: test error due to NewServer param change

--- api/users_test.go
+++ api/users_test.go
@@ -96,7 +96,7 @@ func TestGetUser(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/users/%s", tc.username)
@@ -266,7 +266,7 @@ func TestCreateUserAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			bodyByes, err := json.Marshal(tc.body)
